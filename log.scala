a468d41aad3df004addfb310774c31246d38faff
Romanova-Svetlana <48834242+Romanova-Svetlana@users.noreply.github.com>
2022-06-09 04:38:41 +0300

    Log
    outputting a message to the terminal and creating a log file

---
+++ log.scala
@@ -0,0 +1,36 @@
+import java.time.{LocalDate, LocalTime}
+import java.io.PrintWriter
+import scala.util.{Try,Success,Failure}
+
+  val logPath = "" // absolute path to the log files directory
+
+  // res - info, warn, error or other
+  // mes - any message
+  // file - save or not to file
+  // err - bug report
+  def log(res: String, msg: String, file: Boolean = false, err: List[Any] = List()) = {
+    println(s"[$res] $msg")
+    for (e <- err) println(s"[$res] $e")
+    if (file == true) {
+      val m = msg.take(50)
+      val logFile = logPath + s"[$res] $m.txt"
+      saveFile(logFile, List(s"[$res] $msg", dateTimeNow) ::: err)
+    }
+  }
+
+  def dateTimeNow = LocalDate.now.toString + " " + LocalTime.now.toString
+
+  // saving data to a file on disk
+  def saveFile[T](path: String, text: List[T]) = try { 
+      val out = new PrintWriter(path) 
+      try {
+        for (t <- text) out.println(t)
+        Success(path)
+      } catch {
+        case err : Throwable => Failure(err)
+      } finally { 
+        out.close() 
+      }
+    } catch {
+      case err : Throwable => Failure(err)
+    }
